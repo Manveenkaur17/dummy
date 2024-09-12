f7a914065314983e29a85224001d69278e3c9f1c
Manveenkaur17 <136910166+Manveenkaur17@users.noreply.github.com>
2024-09-12 15:02:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,7 @@
+// App.js
+import React from 'react';
+import Navigation from './android/app/src/Navigation/Navigation'; // Adjust the import path if necessary
+
+export default function App() {
+  return <Navigation />;
+}
